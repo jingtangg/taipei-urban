a9e3417d2c958688ed5204bfc7852db09049a070
jingtangg <jingtang1927@gmail.com>
2026-04-09 10:13:27 +0800

    chore:清除用不到的api

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\Base\DistrictController;
 use App\Http\Controllers\Base\FireHydrantController;
 use App\Http\Controllers\Base\FireStationController;
 use App\Http\Controllers\Base\RoadPlannedController;
-use App\Http\Controllers\Base\RoadMeasuredController;
+
 use App\Http\Controllers\Base\NarrowAlleyController;
 use App\Http\Controllers\Base\DashboardController;
 
@@ -17,7 +17,7 @@ Route::get('/districts/metadata', [DistrictController::class, 'metadata']);
 Route::get('/fire-hydrants', [FireHydrantController::class, 'index']);
 Route::get('/fire-stations', [FireStationController::class, 'index']);
 Route::get('/roads/planned', [RoadPlannedController::class, 'index']);
-Route::get('/roads/measured', [RoadMeasuredController::class, 'index']);
+
 Route::get('/narrow-alleys', [NarrowAlleyController::class, 'index']);
 Route::get('/dashboard/narrow-alley-statistics', [DashboardController::class, 'narrowAlleyStatistics']);
 Route::get('/dashboard/district-rankings', [DashboardController::class, 'districtRankings']);
